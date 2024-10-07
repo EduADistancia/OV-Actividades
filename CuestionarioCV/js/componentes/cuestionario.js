da2695089e57c0f697c5eea5b7e952ae319c48cf
ChTole <ch.ariel.tole@gmail.com>
2024-10-07 13:07:43 -0300

    Cuestionario CV

---
+++ CuestionarioCV/js/componentes/cuestionario.js
@@ -0,0 +1,68 @@
+function crearCuestionario(datos){
+    let contenidoDinamico = document.querySelector('#contenidoDinamico');
+
+    let divItem;
+    let respuestasUl;
+    let pregunta;
+    let devolucionDiv;
+    let contadorId = 0;
+
+    for (let d of datos) {
+        
+
+        if (d["Tipo"] === "Pregunta"){
+            pregunta = d["Texto"];
+            divItem = document.createElement('div');
+            divItem.className = 'marco';
+            let preguntaP = document.createElement('p');
+            preguntaP.innerHTML = d["Texto"];
+            divItem.append(preguntaP);
+            respuestasUl = document.createElement('ul');
+            devolucionDiv = document.createElement('div');
+
+        } else if (d["Tipo"] === "Respuesta") {
+            let rta = document.createElement('li');
+            let chBox = document.createElement('input');
+            chBox.type = d["Etiqueta"];
+            chBox.name = pregunta
+            chBox.id = `${d["Respuesta"] === "No" ? d["Respuesta"] : "Si"}${contadorId}`;
+            
+            let etiqRta = document.createElement('label');
+            etiqRta.setAttribute('for', `${d["Respuesta"]  === "No" ? d["Respuesta"] : "Si"}${contadorId}`);
+            etiqRta.innerHTML = d["Texto"]
+            
+            let devolucionP = document.createElement('p');
+            devolucionP.classList = 'marco oculto';
+            devolucionP.setAttribute('name', `${d["Respuesta"] === "No" ? d["Respuesta"] : "Si"}${contadorId}`);
+            devolucionP.innerText = d["Devolucion"];
+
+            let img = document.createElement('img');
+        
+            if (d["Estado"]) {
+                img.setAttribute('src', './img/correcto.png');
+                img.setAttribute('alt', 'Correcto');
+                devolucionP.classList.add('marcoOK');
+            } else {
+                img.setAttribute('src', './img/incorrecto.png');
+                img.setAttribute('alt', 'Incorrecto');
+                devolucionP.classList.add('marcoNoOk');
+            }
+            
+            img.setAttribute('name', `${d["Texto"] === "No" ? d["Texto"] : "Si"}${contadorId}`);
+            img.className = 'imgEstado oculto';
+
+            devolucionDiv = document.createElement('div');
+
+            devolucionDiv.append(devolucionP, img);
+            rta.append(chBox, etiqRta, devolucionDiv);
+            respuestasUl.append(rta);
+            divItem.append(respuestasUl);
+        }
+
+        contenidoDinamico.append(divItem);
+        contadorId++;
+    }
+
+};
+
+export { crearCuestionario }
